d0b97345cb4231ed31a18b8ed7bd55f3d249c573
isarbru <isabelabruzinga@gmail.com>
2021-06-14 19:08:46 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,89 @@
+let url = "https://tt905a.herokuapp.com/personagens/"
+
+async function callFetchWithGet(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+    const output = document.getElementById("json");
+    const response = await fetch(url, options);
+
+    if (response.status >= 200 && response.status <= 300){
+        console.log("Funcionou");
+        output.innerHTML = await response.text();
+    } else {
+        console.log("Deu errado");
+    }
+}
+
+async function callFetchWithPost(nome, idade, paisorigem){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({'personagem':{
+            'nome' : nome,'idade':idade, 'pais origem':paisorigem
+        }})
+    }
+    await fetch(url, options);
+}
+
+async function callFetchWithPut(id, nome){
+    const options = {
+        method : 'PUT',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'            
+        }, 
+        body :JSON.stringify({'personagem':{
+            'nome' : nome
+        }})
+    }
+    await fetch(`${url}${id}`, options);
+}
+
+async function callFetchWithDelete(id){
+    const options = {
+        method : 'DELETE',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json' 
+        }
+    }
+    await fetch(`${url}${id}`, options);
+}
+
+/*
+    Formulários
+*/
+
+function submitPost(){
+    const form = document.forms['postForm'];    
+    const nome = form["nome"].value;
+    const idade = form["idade"].value;
+    const pais_origem = form["pais_origem"].value;
+    callFetchWithPost(nome, idade, pais_origem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitPut(){
+    const form = document.forms['putForm'];  
+    const id = form["id"].value;  
+    const nome = form["nome"].value;
+    callFetchWithPut(id, nome);
+    return false; // Evitar o reload da tela.
+}
+
+function submitDelete(){
+    const form = document.forms['deleteForm'];  
+    const id = form["id"].value;  
+    callFetchWithDelete(id);
+    return false; // Evitar o reload da tela.
+}
